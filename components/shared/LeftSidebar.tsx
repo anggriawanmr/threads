6c133fa7d6ce808e7054d2ec3e269ab51b35cf04
anggriawanmr <anggriawanmr@gmail.com>
2024-02-29 10:01:10 +0700

    Adding index.js in constants :sparkles:

--- components/shared/LeftSidebar.tsx
+++ components/shared/LeftSidebar.tsx
@@ -1,5 +1,11 @@
 function LeftSidebar() {
-  return <h1>LeftSidebar</h1>;
+  return (
+    <section className="custom-scrollbar lefsidebar">
+      <div className="flex w-full flex-1 flex-col gap-6 px-6">
+        <Link></Link>
+      </div>
+    </section>
+  );
 }
 
 export default LeftSidebar;
