0a4da3f12848634bc6b4251006e2473fb30d4510
anggriawanmr <anggriawanmr@gmail.com>
2024-03-01 09:31:55 +0700

    Editiing LeftSidebar.tsx in shared :hammer:

--- components/shared/LeftSidebar.tsx
+++ components/shared/LeftSidebar.tsx
@@ -1,8 +1,17 @@
+import { sidebarLinks } from "@/constants";
+import Image from "next/image";
+import Link from "next/link";
+
 function LeftSidebar() {
   return (
     <section className="custom-scrollbar lefsidebar">
       <div className="flex w-full flex-1 flex-col gap-6 px-6">
-        <Link></Link>
+        {sidebarLinks.map((link) => (
+          <Link href={link.route} key={link.label} className="leftsidebar_link">
+            <Image src={link.imgURL} alt={link.label} width={24} height={24} />
+            <p className="text-light-1 max-lg:hidden"></p>
+          </Link>
+        ))}
       </div>
     </section>
   );
