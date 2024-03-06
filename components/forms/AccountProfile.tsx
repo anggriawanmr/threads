5ad68d9794a32ba6baae5b80fff145b128f37d0e
anggriawanmr <anggriawanmr@gmail.com>
2024-03-06 11:46:10 +0700

    Editing AccountProfile component :hammer:

--- components/forms/AccountProfile.tsx
+++ components/forms/AccountProfile.tsx
@@ -10,9 +10,12 @@ import {
   FormLabel,
   FormMessage,
 } from "@/components/ui/form";
+import { z } from "zod";
 
 import { zodResolver } from "@hookform/resolvers/zod";
 import { UserValidation } from "@/lib/validations/user";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
 
 interface Props {
   user: {
@@ -37,7 +40,35 @@ const AccountProfile = ({ user, btnTitle }: Props) => {
     },
   });
 
-  return <Form>Account Profile</Form>;
+  function onSubmit(values: z.infer<typeof UserValidation>) {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    console.log(values);
+  }
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+        <FormField
+          control={form.control}
+          name="username"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Username</FormLabel>
+              <FormControl>
+                <Input placeholder="shadcn" {...field} />
+              </FormControl>
+              <FormDescription>
+                This is your public display name.
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit">Submit</Button>
+      </form>
+    </Form>
+  );
 };
 
 export default AccountProfile;
